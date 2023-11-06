0d625b08c86b521d20ac3565ee6c97a90ccfdbcf
davide.morelli <davide.morelli@edu.hefr.ch>
2023-11-06 09:45:25 +0100

    Added pre and post conditions to the first function

--- Demo/cmake-build-debug/cmake_install.cmake
+++ Demo/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/davide/Library/CloudStorage/OneDrive-UniversitédeFribourg/Dependable Systems/P1
+# Install script for directory: /Users/davide/Library/CloudStorage/OneDrive-UniversitédeFribourg/Dependable Systems/Workspace/Demo
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/davide/Library/CloudStorage/OneDrive-UniversitédeFribourg/Dependable Systems/P1/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/davide/Library/CloudStorage/OneDrive-UniversitédeFribourg/Dependable Systems/Workspace/Demo/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
